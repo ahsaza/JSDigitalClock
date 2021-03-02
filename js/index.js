a78155e3a0970033366acadc641cd6f9d794c29d
ahsaza <79809674+ahsaza@users.noreply.github.com>
2021-03-03 01:01:34 +0200

    Applied new learned code for more readability and simplicity

--- js/index.js
+++ js/index.js
@@ -1,14 +1,20 @@
-const hours = document.getElementById("hours");
-const minutes = document.getElementById("minutes");
-const seconds = document.getElementById("seconds");
-const am = document.getElementById("am");
-const pm = document.getElementById("pm");
-setInterval(() => {
-    const currentHours = new Date().getHours();
-    const currentMinutes = new Date().getMinutes();
-    const currentSeconds = new Date().getSeconds();
-    hours.innerText = currentHours < 10 ? "0" + currentHours.toString() : currentHours > 12 ? (currentHours - 12) : currentHours;
-    minutes.innerText = currentMinutes < 10 ? "0" + currentMinutes.toString() : currentMinutes;
-    seconds.innerText = currentSeconds < 10 ? "0" + currentSeconds.toString() : currentSeconds;
-    currentHours > 12 ? (pm.style["display"] = "block", am.style["display"] = "none") : (pm.style["display"] = "none", am.style["display"] = "block");
-}, 1000)
+const clock = document.getElementById("clock");
+
+const time = () => {
+	const date = new Date();
+	const currentHour =
+		date.getHours() < 10
+			? "0" + date.getHours()
+			: date.getHours() > 12
+			? date.getHours() - 12
+			: date.getHours();
+	const currentMinutes =
+		date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+	const currentSeconds =
+		date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+	let amPm = currentHour >= 12 ? "Pm" : "Am";
+
+	clock.innerText = `${currentHour}:${currentMinutes}:${currentSeconds} ${amPm}`;
+};
+
+setInterval(() => time(), 1000);
